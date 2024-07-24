e498456763aef6d89c4321c882e27c54276c4bac
tom <tom@opensystemslab.io>
2024-07-24 15:13:49 +0100

    wip: more fixes in core + routing

--- app/app.tsx
+++ app/app.tsx
@@ -25,9 +25,9 @@ const App = () => {
       if (polygon !== null) updateLocatePolygon(polygon);
 
       switch (true) {
-        case polygon !== null && houses && houses.length > 0:
-          router.push("build");
-          return;
+        // case polygon !== null && houses && houses.length > 0:
+        //   router.push("build");
+        //   return;
         case polygon !== null:
           router.push("design");
           return;
