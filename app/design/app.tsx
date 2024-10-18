397a272f07f8875e21a250704c834aed8b5fd160
tom <tom@opensystemslab.io>
2024-10-18 10:19:55 +0100

    wip

--- app/design/app.tsx
+++ app/design/app.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Add } from "@carbon/icons-react";
+import { Add, Reset, View, WatsonHealthSubVolume } from "@carbon/icons-react";
 import type {
   SceneContextMode,
   ScopeElement,
@@ -12,25 +12,27 @@ import {
   useProjectData,
   useSuspendAllBuildData,
 } from "@opensystemslab/buildx-core";
+import { SharingWorkerUtils } from "@opensystemslab/buildx-core/worker-utils";
+import { formatDistanceToNow } from "date-fns";
 import { pipe } from "fp-ts/lib/function";
+import { useRouter } from "next/navigation";
 import { Suspense, useEffect, useRef, useState } from "react";
 import usePortal from "react-cool-portal";
 import FullScreenContainer from "~/ui/FullScreenContainer";
 import IconButton from "~/ui/IconButton";
-import { Menu } from "~/ui/icons";
-import { A, TE } from "~/utils/functions";
+import { Menu, SectionCuts } from "~/ui/icons";
+import { A, R, TE } from "~/utils/functions";
 import Loader from "../ui/Loader";
-import BuildXContextMenu from "./menu/BuildXContextMenu";
-import ObjectsSidebar from "./ui/objects-sidebar/ObjectsSidebar";
+import Radio from "../ui/Radio";
 import useSharingWorker from "../utils/workers/sharing/useSharingWorker";
-import { SharingWorkerUtils } from "@opensystemslab/buildx-core/worker-utils";
-import ExitMode from "./ui/ExitMode";
-import MetricsWidget from "./ui/metrics/MetricsWidget";
+import BuildXContextMenu from "./menu/BuildXContextMenu";
 import Breadcrumbs from "./ui/Breadcrumbs";
-import { Router } from "next/router";
-import { useRouter } from "next/navigation";
+import Checklist from "./ui/Checklist";
+import ExitMode from "./ui/ExitMode";
+import IconMenu from "./ui/IconMenu";
+import MetricsWidget from "./ui/metrics/MetricsWidget";
+import ObjectsSidebar from "./ui/objects-sidebar/ObjectsSidebar";
 import { getModeUrl } from "./util";
-import { formatDistanceToNow } from "date-fns";
 
 let scene: BuildXScene | null = null;
 
@@ -190,6 +192,104 @@ const SuspendedApp = () => {
           </IconButton>
         </div>
       </HeaderEndPortal>
+
+      <div className="absolute left-0 top-1/2 z-10 flex -translate-y-1/2 transform flex-col justify-center bg-white shadow">
+        {/* <IconMenu icon={() => <ChoroplethMap size={24} className="m-auto" />}>
+          <Radio
+            id="map"
+            label="Map"
+            options={[
+              {
+                label: "Disabled",
+                value: false,
+              },
+              {
+                label: "Enabled",
+                value: true,
+              },
+            ]}
+            selected={mapboxEnabled}
+            onChange={setMapboxEnabled}
+          />
+        </IconMenu> */}
+        <IconMenu icon={() => <View size={24} className="m-auto" />}>
+          <Radio
+            id="camera"
+            label="Camera"
+            options={[
+              {
+                label: "Perspective",
+                value: false,
+              },
+              {
+                label: "Orthographic",
+                value: true,
+              },
+            ]}
+            selected={orthographic}
+            onChange={setOrthographic}
+          />
+          <IconButton onClick={cameraReset}>
+            <Reset size={24} className="m-auto" />
+          </IconButton>
+        </IconMenu>
+
+        <IconMenu icon={SectionCuts}>
+          <Checklist
+            label="Vertical cuts"
+            options={[
+              { value: "width", label: "Width" },
+              { value: "length", label: "Length" },
+            ]}
+            selected={pipe(
+              verticalCuts,
+              R.filter((x) => x),
+              keys
+            )}
+            onChange={setVerticalCuts}
+          />
+          <Radio
+            id="ground-plane"
+            label="Ground Plane"
+            options={[
+              { value: false, label: "None" },
+              { value: true, label: "Regular" },
+            ]}
+            selected={groundPlane}
+            onChange={(newValue) => {
+              setGroundPlaneEnabled(newValue);
+            }}
+          />
+        </IconMenu>
+        <IconMenu
+          icon={() => <WatsonHealthSubVolume size={24} className="m-auto" />}
+        >
+          <Checklist
+            label="Building elements"
+            options={pipe(
+              categories,
+              R.collect(S.Ord)((label, value) => ({ label, value: label }))
+            )}
+            selected={pipe(
+              categories,
+              R.filter((x) => x),
+              R.collect(S.Ord)((value) => value)
+            )}
+            onChange={(selectedCategories) =>
+              pipe(
+                elementCategories,
+                R.collect(S.Ord)((k, b) => {
+                  if (selectedCategories.includes(k)) {
+                    if (!elementCategories[k]) elementCategories[k] = true;
+                  } else {
+                    if (elementCategories[k]) elementCategories[k] = false;
+                  }
+                })
+              )
+            }
+          />
+        </IconMenu>
+      </div>
 
       <ObjectsSidebar
         expanded={objectsSidebar}
