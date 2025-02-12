3ec666884838a0d046aa3b350d5c4e6ad776b359
tom <tom@opensystemslab.io>
2025-02-12 13:21:48 +0000

    wip: #2

--- app/design/ui/InteractionHint.tsx
+++ app/design/ui/InteractionHint.tsx
@@ -11,19 +11,17 @@ const InteractionHint = ({ show }: InteractionHintProps) => {
     internalShowHide: false,
   });
 
-  console.log({ show });
-
   if (!show) return null;
 
   return (
     <Portal>
-      <div className="absolute left-1/2 top-24 z-50 flex -translate-x-1/2 transform justify-center">
+      <div className="absolute left-1/2 top-32 z-50 flex -translate-x-1/2 transform justify-center">
         <div className="flex justify-between items-center h-12 rounded-full bg-white p-2 shadow-lg">
+          <span className="ml-6 mr-2">
+            Right click or long tap to explore interactions
+          </span>
           <span className="w-8 p-1">
             <Information size="24" />
-          </span>
-          <span className="mx-6">
-            Right click or long tap to explore interactions
           </span>
         </div>
       </div>
